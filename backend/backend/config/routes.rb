4ad0b37231d727b35a43fe4637f146ff14695cbf
yui <yuixicun97@gmail.com>
2026-03-25 02:34:56 +0900

    ととのえた

--- backend/backend/config/routes.rb
+++ backend/backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: proc { [200, {}, ["OK"]] }
+
   resources :events
   resources :people
 end
--- backend/backend/test/models/person_event_test.rb
+++ backend/backend/test/models/person_event_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PersonEventTest < ActiveSupport::TestCase
+class EventPersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
