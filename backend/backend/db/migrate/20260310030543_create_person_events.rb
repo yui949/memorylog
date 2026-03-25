7df2fc4a4f0fbc575da44162f8b88189a9b6855f
yui <yuixicun97@gmail.com>
2026-03-25 18:16:01 +0900

    変更

--- backend/backend/db/migrate/20260310030543_create_person_events.rb
+++ backend/backend/db/migrate/20260310030543_create_person_events.rb
@@ -1,4 +1,4 @@
-class CreateEventPerson < ActiveRecord::Migration[7.0]
+class CreatePersonEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :person_events do |t|
       t.references :person, null: false, foreign_key: true
