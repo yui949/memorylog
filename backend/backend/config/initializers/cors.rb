8cb8903fbdd55fa7fa157164f51b89995c0e3cf8
yui <yuixicun97@gmail.com>
2026-03-25 20:47:26 +0900

    フロントとバックのURL書く

--- backend/backend/config/initializers/cors.rb
+++ backend/backend/config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://memorylog-frontend.vercel.app'
+    origins 'https://memorylog-frontend.vercel.app',
+            'https://memorylog-frontend-git-new-main-yui949s-projects.vercel.app'
 
     resource '*',
       headers: :any,
