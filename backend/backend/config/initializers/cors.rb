5563dad84b5e4b1ef3aabe7e63726e1389fa6cca
yui <yuixicun97@gmail.com>
2026-03-25 14:14:05 +0900

    URL変更

--- backend/backend/config/initializers/cors.rb
+++ backend/backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'   # 開発中は全部許可、公開時はドメイン指定にする
+    origins 'https://memorylog-frontend.vercel.app'   # 開発中は全部許可、公開時はドメイン指定にする
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
